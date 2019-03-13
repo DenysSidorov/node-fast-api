32aeb85f014222a9ac6f8c2ea57f89d761058aac
Denys Sidorov <000scorpions000@gmail.com>
2019-03-14 00:04:09 +0200

    add EventEmmiter example

--- learn-modules/modules/index.js
+++ learn-modules/modules/index.js
@@ -1,2 +1,55 @@
-var  path  = require ('path');
-console.log(path.basename(__));
+var EventEmmiter = require('events');
+
+class MyCl extends EventEmmiter {
+
+  constructor(args) {
+    super(args);
+    this.count = 0;
+  }
+
+  start() {
+
+    var self = this;
+    this.ownInterval = setInterval(function () {
+        self.tick();
+      },
+      100
+    );
+    this.emit('start');
+  }
+
+  tick() {
+    this.count++;
+    if (this.count <= 10) {
+      this.emit('data', this.count);
+
+    } else {
+      this.end();
+    }
+
+
+  }
+
+  end() {
+    clearInterval(this.ownInterval);
+    this.emit('end');
+  }
+}
+
+var myInstance = new MyCl();
+
+myInstance.start();
+
+myInstance.on('start', function () {
+  console.log('STAAAART');
+})
+
+myInstance.on('end', function () {
+  console.log('EEENNNDD');
+})
+
+myInstance.on('data', function (data) {
+  console.log(data);
+})
+
+module.exports = MyCl;
